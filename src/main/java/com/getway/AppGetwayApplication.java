dde2226ac74f9e6a8045bb6ff21e455bb8186e94
TheMrVikas <vikas83499@gmail.com>
2022-02-20 22:43:31 +0530

    initial commit

---
+++ src/main/java/com/getway/AppGetwayApplication.java
@@ -0,0 +1,13 @@
+package com.getway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGetwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppGetwayApplication.class, args);
+	}
+
+}
